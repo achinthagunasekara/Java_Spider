0883eff980ae79fad93fb10382cd3888a8bbf042
Archie Gunasekara <achintha@me.com>
2015-11-19 14:16:02 +1100

    restricted to only support os x

--- src/JavaSpider.java
+++ src/JavaSpider.java
@@ -7,10 +7,21 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class JavaSpider {
     
     public static void main(String[] args) {
+        
+        String[] supportedOSs = { "mac os x" };
+        String osName = System.getProperty("os.name").toLowerCase();
+        
+        if(!Arrays.asList(supportedOSs).contains(osName)) {
+            
+            JOptionPane.showMessageDialog(null, "Your OS '" + osName + "' is not supported", "OS Not Supported", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
         JFrame frame = new JFrame("Train Demo");
         frame.setUndecorated(true);
         frame.setBackground(new Color(0, 0, 0, 0));
