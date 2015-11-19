59befaccf6e05eaed722b4b360f26f742bb22e9c
Archie Gunasekara <achintha@me.com>
2015-11-19 14:03:06 +1100

    moving spider on virtially as well

--- src/SpiderCanvas.java
+++ src/SpiderCanvas.java
@@ -9,11 +9,13 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.Random;
 import javax.swing.JComponent;
 
 public class SpiderCanvas extends JComponent {
 
     private int lastXPos = 0;
+    private int lastYPos = 0;
 
     public SpiderCanvas() {
         Thread animationThread;
@@ -32,11 +34,17 @@ public class SpiderCanvas extends JComponent {
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D gg = (Graphics2D) g;
+        Random rand = new Random();
 
         int windowWidth = getWidth();
         int windowHeight = getHeight();
         int spiderWidth = 20;
         int spiderSpeed = 3;
+        
+        if(lastYPos == 0 || lastXPos == -spiderWidth) {
+            lastYPos = rand.nextInt(windowWidth-0) + 0;
+        }
+        
 
         int xPos = lastXPos + spiderSpeed;
 
@@ -45,7 +53,7 @@ public class SpiderCanvas extends JComponent {
         }
 System.out.println(xPos);
         Image img1 = Toolkit.getDefaultToolkit().getImage("images/spider.gif");
-        gg.drawImage(img1, xPos, windowHeight/2, this);
+        gg.drawImage(img1, xPos, lastYPos, this);
 
         lastXPos = xPos;
     }
